6494432db3f2ba27a8de31227bfd460c09e1a8a1
Garushisu <akenoryuusei2@yahoo.co.jp>
2023-12-15 09:51:05 +0900

    completed setting patch features

--- Application about print/OsuRun.cs
+++ Application about print/OsuRun.cs
@@ -24,9 +24,9 @@ namespace Application_about_print
             this.Load += OsuRun_Load;
         }
 
-        private async void button3_Click(object sender, EventArgs e)
+        private void button3_Click(object sender, EventArgs e)
         {
-            if(!string.IsNullOrEmpty(Properties.Settings.Default.ReplayPath))
+            if (!string.IsNullOrEmpty(Properties.Settings.Default.ReplayPath))
             {
                 System.Diagnostics.Process.Start(Properties.Settings.Default.ReplayPath);
             } else
@@ -41,7 +41,7 @@ namespace Application_about_print
                 {
                 }
             }
-            
+
         }
 
         private void OsuRun_Load(object sender, EventArgs e)
@@ -84,7 +84,7 @@ namespace Application_about_print
             {
 
             }
-            
+
 
         }
 
@@ -95,44 +95,44 @@ namespace Application_about_print
 
         private void timer1_Tick(object sender, EventArgs e) // Enable を true にすると ループ してくれる
         {
-            
+
             string replayFolder = Properties.Settings.Default.ReplayPath;
 
-                if (!string.IsNullOrEmpty(Properties.Settings.Default.ReplayPath))
+            if (!string.IsNullOrEmpty(Properties.Settings.Default.ReplayPath))
+            {
+                if (replayMoves.Checked == true)
                 {
-                    if (replayMoves.Checked == true)
+                    string[] files = Directory.GetFiles(replayFolder);
+                    foreach (string file in files)
                     {
-                        string[] files = Directory.GetFiles(replayFolder);
-                        foreach (string file in files)
+                        try
                         {
-                            try
-                            {
-                                Process.Start(file);
+                            Process.Start(file);
 
-                                Thread.Sleep(2000);
+                            Thread.Sleep(2000);
 
-                                File.Delete(file);
-                            }
-                            catch (Exception ex)
-                            {
-                                Console.WriteLine("ファイルは存在しません。" + ex.Message);
-                            }
+                            File.Delete(file);
                         }
-                    }
-                    else
-                    {
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine("ファイルは存在しません。" + ex.Message);
+                        }
                     }
                 }
                 else
                 {
-
                 }
-            
+            }
+            else
+            {
 
-            
-                
+            }
 
-            
+
+
+
+
+
         }
 
         private void button1_Click_2(object sender, EventArgs e)
@@ -144,7 +144,7 @@ namespace Application_about_print
             }
             else
             {
-                
+
             }
 
 
@@ -186,45 +186,93 @@ namespace Application_about_print
             bool checkPatch1Checked = Properties.Settings.Default.checkPath1;
             if (checkPatch1Checked)
             {
-                ProcessStartInfo psi2 = new ProcessStartInfo(Properties.Settings.Default.patchPath);
-                Process.Start(psi2);
+                if (!string.IsNullOrEmpty(Properties.Settings.Default.patchPath))
+                {
+                    ProcessStartInfo psi2 = new ProcessStartInfo(Properties.Settings.Default.patchPath);
+                    Process.Start(psi2);
 
                     await Task.Delay(2000);
 
-                ProcessStartInfo psi = new ProcessStartInfo(Properties.Settings.Default.Path1);
-                Process.Start(psi);
+                    ProcessStartInfo psi = new ProcessStartInfo(Properties.Settings.Default.Path1);
+                    Process.Start(psi);
+                } else
+                {
+                    MessageBox.Show("You didn't setting Patcher's path, it'll run osu! without Patcher.");
+
+                    ProcessStartInfo psi = new ProcessStartInfo(Properties.Settings.Default.Path1);
+                    Process.Start(psi);
+                }
+
             } else {
                 ProcessStartInfo psi = new ProcessStartInfo(Properties.Settings.Default.Path1);
                 Process.Start(psi);
             }
-            
+
         }
 
         //ProcessStartInfo psi2 = new ProcessStartInfo("F:\\フォルダー\\akatsuki.exe");
         //Process.Start(psi2);
 
-            //await Task.Delay(2000);
+        //await Task.Delay(2000);
 
         //ProcessStartInfo psi = new ProcessStartInfo("C:\\Users\\akeno\\Desktop\\Apps\\!Mames Server.lnk");
         //Process.Start(psi);
 
-        private void second_Click(object sender, EventArgs e)
+        private async void second_Click(object sender, EventArgs e)
         {
             bool checkPatch2Checked = Properties.Settings.Default.checkPath2;
+
             if (checkPatch2Checked)
             {
-                MessageBox.Show(checkPatch2Checked.ToString());
-            }
-            else
-            {
-                MessageBox.Show(checkPatch2Checked.ToString());
+                if (!string.IsNullOrEmpty(Properties.Settings.Default.patchPath))
+                {
+                    ProcessStartInfo psi2 = new ProcessStartInfo(Properties.Settings.Default.patchPath);
+                    Process.Start(psi2);
+
+                    await Task.Delay(2000);
+
+                    ProcessStartInfo psi = new ProcessStartInfo(Properties.Settings.Default.Path2);
+                    Process.Start(psi);
+                }
+                else
+                {
+                    MessageBox.Show("You didn't set Patcher's path, it'll run osu! without Patcher.");
+
+                    ProcessStartInfo psi = new ProcessStartInfo(Properties.Settings.Default.Path2);
+                    Process.Start(psi);
+                }
             }
         }
 
-        private void third_Click(object sender, EventArgs e)
+        private async void third_Click(object sender, EventArgs e)
         {
-            ProcessStartInfo psi = new ProcessStartInfo(Properties.Settings.Default.Path3);
-            Process.Start(psi);
+            bool checkPath3Checked = Properties.Settings.Default.checkPath3;
+
+            if (checkPath3Checked)
+            {
+                if (!string.IsNullOrEmpty(Properties.Settings.Default.patchPath))
+                {
+                    ProcessStartInfo psi2 = new ProcessStartInfo(Properties.Settings.Default.patchPath);
+                    Process.Start(psi2);
+
+                    await Task.Delay(2000);
+
+                    ProcessStartInfo psi = new ProcessStartInfo(Properties.Settings.Default.Path3);
+                    Process.Start(psi);
+                }
+                else
+                {
+                    MessageBox.Show("You didn't set Patcher's path, it'll run osu! without Patcher.");
+
+                    ProcessStartInfo psi = new ProcessStartInfo(Properties.Settings.Default.Path3);
+                    Process.Start(psi);
+                }
+            }
+            else
+            {
+                ProcessStartInfo psi = new ProcessStartInfo(Properties.Settings.Default.Path1);
+                Process.Start(psi);
+            }
         }
-    }
+    } 
 }
